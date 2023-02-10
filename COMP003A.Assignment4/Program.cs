2cf61fa0d91d33da78fbeca8a3f819adc72145d8
MatthewAlvarez5 <alvarezmatthew2002@gmail.com>
2023-02-09 17:33:47 -0800

    Add project files.

---
+++ COMP003A.Assignment4/Program.cs
@@ -0,0 +1,61 @@
+﻿/* 
+ *  Author: Matthew Alvarez
+ *  Course: COMP-003A
+ *  Purpose: Assignment 4 - Looping Structures
+ */
+
+namespace COMP003A.Assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "COMP-003A - Assignment 4";
+
+            /* for-loop section (Right Triangle) */
+            Console.WriteLine("".PadRight(50,'-'));
+            Console.WriteLine("\tfor-loop section");
+            Console.WriteLine("".PadRight(50, '-'));
+            Console.Write("Enter a positive whole number: ");
+            int limit = Convert.ToInt32(Console.ReadLine());
+
+            string output = "#";
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
